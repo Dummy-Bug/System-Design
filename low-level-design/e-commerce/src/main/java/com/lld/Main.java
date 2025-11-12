fd4f410fb302818c4f94c4f2bc5fccc347c06f4f
LaxitRana <laxyrajput@gmail.com>
2025-11-12 22:26:24 +0530

    adding other artifacts

---
+++ low-level-design/e-commerce/src/main/java/com/lld/Main.java
@@ -0,0 +1,37 @@
+package com.lld;
+
+import com.lld.criteria.BrandFilterCriteria;
+import com.lld.criteria.Criteria;
+import com.lld.criteria.PriceFilterCriteria;
+import com.lld.factories.PriceComparisonStrategyFactory;
+import com.lld.models.Brand;
+import com.lld.models.Category;
+import com.lld.models.Product;
+import com.lld.utils.Operator;
+
+import java.util.List;
+
+public class Main {
+    static void main() {
+        Brand b1 = new Brand("Apple");
+        Brand b2 = new Brand("Samsung");
+        Brand b3 = new Brand("OnePlus");
+
+        Category c1 = new Category("Electronics");
+
+        Product p1 = new Product("iPhone 12", 799.99, b1, c1);
+        Product p2 = new Product("Galaxy S21", 1000.00, b2, c1);
+        Product p3 = new Product("OnePlus 9", 699.99, b3, c1);
+
+        List<Product> products = List.of(p1, p2, p3);
+
+        Criteria cr1 = new BrandFilterCriteria("Apple");
+        Criteria cr2 = new BrandFilterCriteria("Samsung");
+
+        Criteria priceFilterCriteria = new PriceFilterCriteria(999, PriceComparisonStrategyFactory.create(Operator.LESS_THAN));
+        List<Product> priceFilteredProducts = priceFilterCriteria.satisfy(products);
+        for (Product p : priceFilteredProducts) {
+            IO.println(p.getName());
+        }
+    }
+}
